5c82cb9b976ffc837524992fae28330105b7a90b
Stephanie Andrews <seandrews11@gmail.com>
2017-01-18 18:31:37 -0700

    completed

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,36 @@
+function postComments(state = [], action) {
+  console.log('yo')
+  switch(action.type) {
+    case 'ADD_COMMENT':
+    // return existing state and new comment
+    return [...state, {
+      user: action.author,
+      text: action.comment
+    }];
+    case 'REMOVE_COMMENT':
+    // return state without deleted comment
+    return [
+      //from the start to the one we want to delete
+      ...state.slice(0, action.i),
+      //after the deleted one to the end
+      ...state.slice(action.i + 1)
+    ]
+    default:
+      return state;
+  }
+  return state;
+}
+
+function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined'){
+    return {
+      // take current state
+      ...state,
+      //overwrite post with new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
+}
+
+export default comments;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,47 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+import css from './styles/style.styl';
+
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, {history} from './store';
+
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: 'asdfas9d08f',
+    userLevel: 'editor'
+  }
+}).install();
+
+// logException(new Error('download failed'), {
+//   Email: 'seandrews11@gmail.com'
+// });
+
+// good if you don't want all the info
+// Raven.captureMessage('something bad happenned')
+// let user give feedback
+// Raven.showReportDialog()
+
+const router = (
+  <Provider store={store}>
+    <Router history = {history}>
+      <Route path = "/" component={App}>
+        <IndexRoute component = {PhotoGrid}></IndexRoute>
+        <Route path ="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+
+)
+
+
+render(router, document.getElementById('root'));
